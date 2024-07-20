33829342327b90856f197dba004bb73b958b181a
VladSH33 <vlad.shevkunov33@gmail.com>
2024-07-20 20:28:50 +0200

    class Page

--- js/script.js
+++ js/script.js
@@ -3,52 +3,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Класс страницы
 
-    // class Page {
-    //     constructor(){
+    class Page {
+        constructor (API, bodyContainer) {
+            this.API = API;
+            this.bodyContainer = bodyContainer;
+        }
 
-    //     }
-    // }
+        static Card = class  {
+            constructor (card, parentSelector) {
+                this.card = card;
+                this.parent = document.querySelector(parentSelector);
+            }
+
+            //создание карточки
+            render() {
+
+                // создаю элементы
+
+                const cardContainer = document.createElement('div');
+                const specificationsCard = document.createElement('div');
+                const titleCard = document.createElement('div');
+
+                //Присваиваю классы
+
+                cardContainer.classList.add('card');
+                specificationsCard.classList.add('card__specifications');
+                titleCard.classList.add('card__title')
+
+                    //Преобразую массив с обьектами в Массив с подмассивами, и получаю подмассив с name
+
+                    const specifications = Object.entries(this.card)
+                    const specificationsTitle = specifications.find(title => title[0] == 'name')
+                    const foundTitle = specificationsTitle ? specificationsTitle[1] : undefined;
+
+                    //обрезаю лишине данные
+
+                    const specificationsSlice = specifications.slice(1, 8);
+    
+                    specificationsSlice.forEach(([key, value]) => {
+                        //заменяю _ в названии характеристик на пробел
+                        const specificationName = key.replace('_',' ');
+                        //формирую элементы с классами, названием характеристики и значением
+                        specificationsCard.innerHTML += 
+                        `
+                            <div class="${key}">${specificationName}: ${value}</div>
+                        `
+                    })
+                
+                //Передаю переменные в элементы на странице
+
+                titleCard.append(foundTitle)
+                cardContainer.append(titleCard)
+                cardContainer.append(specificationsCard)
+                this.parent.append(cardContainer)
+            }
+    
+            remove() {
+                //удаляет карточки для отрисовки других карточек
+            }
+        }
+    
+    }
 
 // Класс карточки
-    class Card {
-        constructor(card, parentSelector) {
-            this.card = card;
-            this.parent = document.querySelector(parentSelector);
-        }
-//создание карточки
-        render() {
-            // создаю элементы
-            const cardContainer = document.createElement('div');
-            const specificationsCard = document.createElement('div');
-            const titleCard = document.createElement('div');
-            //Присваиваю классы
-            cardContainer.classList.add('card');
-            specificationsCard.classList.add('card__specifications');
-            titleCard.classList.add('card__title')
-                //Преобразую массив с обьектами в Массив с подмассивами, и получаю подмассив с name
-                const specifications = Object.entries(this.card)
-                const specificationsTitle = specifications.find(title => title[0] == 'name')
-                const getTitle = specificationsTitle ? specificationsTitle[1] : undefined;
-                //обрезаю лишине данные
-                const specificationsSlice = specifications.slice(1, 8);
-
-                specificationsSlice.forEach(([key, value]) => {
-                    //заменяю _ в названии характеристик на пробел
-                    const specificationName = key.replace('_',' ');
-                    //формирую элементы с классами, названием характеристики и значением
-                    specificationsCard.innerHTML += 
-                    `
-                        <div class="${key}">${specificationName}: ${value}</div>
-                    `
-                })
-            
-            //Передаю переменные в элементы на странице
-            titleCard.append(getTitle)
-            cardContainer.append(titleCard)
-            cardContainer.append(specificationsCard)
-            this.parent.append(cardContainer)
-        }
-    }
+    
 
 
 // Получаю данные через fetch
@@ -69,16 +87,21 @@ window.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 0;
 
 //Передаю данные в класс
+
     getResource('https://swapi.dev/api/people/')
     // getResource('https://swapi.dev/api/planets/')
         .then(data => {
             this.cards = data.results.map((card) => {
-                const instance = new Card(card, '.cards');
-                instance.render();
-                return instance;
+              
+                const instanceCards = new Page.Card(card, '.cards');
+                instanceCards.render();
+                return instanceCards;
             });
 
-// Далее работа со слайдером.
+
+
+
+// Далее работа со слайдером
 
             cardsContainer = document.querySelector('.cards');
             cardList = document.querySelectorAll('.card'); // Теперь, когда карточки добавлены, можно получить их коллекцию
